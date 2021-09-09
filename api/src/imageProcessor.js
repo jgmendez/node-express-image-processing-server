d1c8600f026dc84d08fa2288b1b5d4efa93c14dc
Juan Mendez <juan.mendez1@upr.edu>
2021-09-09 13:48:05 -0400

    Completed module

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,25 +1,34 @@
 const path = require("path");
 const { Worker, isMainThread } = require("worker_threads");
-
 const pathToResizeWorker = path.resolve(__dirname, "resizeWorker.js");
 const pathToMonochromeWorker = path.resolve(__dirname, "monochromeWorker.js");
-function imageProcessor(filename) {
+const uploadPathResolver = (filename) => {
+  return path.resolve(__dirname, "../uploads", filename);
+};
+const imageProcessor = (filename) => {
   const sourcePath = uploadPathResolver(filename);
   const resizedDestination = uploadPathResolver(`resized-${filename}`);
   const monochromeDestination = uploadPathResolver(`monochrome-${filename}`);
   let resizeWorkerFinished = false;
   let monochromeWorkerFinished = false;
-
   return new Promise((resolve, reject) => {
     if (isMainThread) {
       try {
-        console.log("resize about to init");
         const resizeWorker = new Worker(pathToResizeWorker, {
-          workerData: { source: sourcePath, destination: resizedDestination },
+          workerData: {
+            source: sourcePath,
+            destination: resizedDestination,
+          },
         });
-        console.log("resize just init");
+
+        const monochromeWorker = new Worker(pathToMonochromeWorker, {
+          workerData: {
+            source: sourcePath,
+            destination: monochromeDestination,
+          },
+        });
+
         resizeWorker.on("message", (message) => {
-          console.log(message);
           resizeWorkerFinished = true;
           if (monochromeWorkerFinished) {
             resolve("resizeWorker finished processing");
@@ -28,29 +37,24 @@ function imageProcessor(filename) {
         resizeWorker.on("error", (error) => {
           reject(new Error(error.message));
         });
+
         resizeWorker.on("exit", (code) => {
           if (code !== 0) {
             reject(new Error(`Exited with status code ${code}`));
           }
         });
-        console.log("mono about to init");
-        const monochromeWorker = new Worker(pathToMonochromeWorker, {
-          workerData: {
-            source: sourcePath,
-            destination: monochromeDestination,
-          },
-        });
-        console.log("mono just init");
+
         monochromeWorker.on("message", (message) => {
-          console.log(message);
           monochromeWorkerFinished = true;
           if (resizeWorkerFinished) {
             resolve("monochromeWorker finished processing");
           }
         });
+
         monochromeWorker.on("error", (error) => {
           reject(new Error(error.message));
         });
+
         monochromeWorker.on("exit", (code) => {
           if (code !== 0) {
             reject(new Error(`Exited with status code ${code}`));
@@ -63,9 +67,6 @@ function imageProcessor(filename) {
       reject(new Error("not on main thread"));
     }
   });
-}
-function uploadPathResolver(filename) {
-  return path.resolve(__dirname, "../uploads", filename);
-}
+};
 
 module.exports = imageProcessor;
--- api/src/router.js
+++ api/src/router.js
@@ -1,35 +1,47 @@
 const { Router } = require("express");
 const multer = require("multer");
-const router = Router();
 const path = require("path");
-function filename(request, file, callback) {
+const imageProcessor = require("./imageProcessor");
+
+const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
+
+const router = Router();
+
+const filename = (request, file, callback) => {
   callback(null, file.originalname);
-}
+};
+
 const storage = multer.diskStorage({
   destination: "api/uploads/",
   filename: filename,
 });
-function fileFilter(request, file, callback) {
+
+const fileFilter = (request, file, callback) => {
   if (file.mimetype !== "image/png") {
     request.fileValidationError = "Wrong file type";
     callback(null, false, new Error("Wrong file type"));
   } else {
     callback(null, true);
   }
-}
-
-const upload = multer({ fileFilter, storage });
+};
+const upload = multer({
+  fileFilter: fileFilter,
+  storage: storage,
+});
 
 router.post("/upload", upload.single("photo"), async (request, response) => {
-  if (request.fileValidationError) {
+  if (request.fileValidationError)
     return response.status(400).json({ error: request.fileValidationError });
-  }
+
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {}
 
   return response.status(201).json({ success: true });
 });
-const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
+
 router.get("/photo-viewer", (request, response) => {
-  return response.sendFile(photoPath);
+  response.sendFile(photoPath);
 });
 
 module.exports = router;
