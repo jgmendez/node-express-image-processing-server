e594632f12501e02e10c0405e40de1190e6fe3c2
Juan Mendez <juan.mendez1@upr.edu>
2021-09-09 13:34:01 -0400

    Completed module

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 const router = require("./src/router");
-pathToIndex = path.resolve(__dirname, "../client/index.html");
+const app = express();
+const pathToIndex = path.resolve(__dirname, "../client/index.html");
 app.use("/", router);
 app.use(express.static(path.resolve(__dirname, "uploads")));
 app.use("/*", (request, response) => {
